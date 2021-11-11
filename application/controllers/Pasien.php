17cda29e30436d2166408c4fbff8f04cf4bb3db1
mpuhambyah <mpu.hambyah@gmail.com>
2021-11-11 16:24:34 +0700

    ts

--- application/controllers/Pasien.php
+++ application/controllers/Pasien.php
@@ -36,23 +36,23 @@ class Pasien extends CI_Controller
     {
         $data['user'] = $this->db->get_where('user', ['id' => $this->session->userdata('id')])->row_array();
         $data['profile'] = $this->db->get('profile')->row_array();
-        $data['title'] = "Rekaman";
+        $data['title'] = "List Pasien";
         $this->load->model('M_dokter');
         $data['id'] = $this->uri->segment(3);
         $data['id_rekaman'] = $this->uri->segment(4);
+        $data['minute'] = $this->uri->segment(5);
         $data['rekamanPasien'] = $this->M_dokter->rekamanPasien($data['id'], $data['id_rekaman']);
         $data['listRekaman'] = $this->M_dokter->listRekaman($data['id']);
-
         $this->load->model('M_data');
         $data['totalData'] = count($this->M_data->dataEcgFull($data['id'], $data['id_rekaman']));
         $data['loopData'] = intval(ceil($data['totalData'] / 800));
-
         $this->load->view('template/header', $data);
         $this->load->view('template/sidebar', $data);
         $this->load->view('template/navbar', $data);
-        $this->load->view('pasien/record', $data);
+        $this->load->view('dokter/record', $data);
         $this->load->view('template/footer', $data);
     }
+
 
     public function listMinute()
     {
