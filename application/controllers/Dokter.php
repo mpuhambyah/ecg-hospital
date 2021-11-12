1f96eaa35003a010d0c0894b2b0a9d0a8f11bc18
mpuhambyah <mpu.hambyah@gmail.com>
2021-11-12 13:29:18 +0700

    ts

--- application/controllers/Dokter.php
+++ application/controllers/Dokter.php
@@ -98,10 +98,12 @@ class Dokter extends CI_Controller
         $data['user'] = $this->db->get_where('user', ['id' => $this->session->userdata('id')])->row_array();
         $data['profile'] = $this->db->get('profile')->row_array();
         $data['title'] = "Profile";
+        $this->load->model('M_data');
+        $data['profileUser'] = $this->M_data->dataProfile($this->session->userdata('id'));
         $this->load->view('template/header', $data);
         $this->load->view('template/sidebar', $data);
         $this->load->view('template/navbar', $data);
-        $this->load->view('dokter/profile', $data);
+        $this->load->view('template/profile', $data);
         $this->load->view('template/footer', $data);
     }
 
