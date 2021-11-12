e671a47fec466e420c37c81b6f208e13faf31bac
mpuhambyah <mpu.hambyah@gmail.com>
2021-11-12 11:04:30 +0700

    ts

--- application/models/M_alat.php
+++ application/models/M_alat.php
@@ -5,7 +5,8 @@ class M_alat extends CI_Model
 {
   public function listPasien()
   {
-    $query = "SELECT pasien.nik, pasien.alamat, pasien.id as 'id', pasien.nama as 'nama', alat.nama as 'nama_alat' FROM pasien 
+    $query = "SELECT pasien.nik, pasien.alamat, pasien.id as 'id', pasien.nama as 'nama', alat.nama as 'nama_alat', user.is_active as 'aktivasi'
+    FROM pasien 
     INNER JOIN alat ON pasien.id_alat = alat.id
     INNER JOIN user ON pasien.NIK = user.email";
 
