9fb8f93eb3320509e256a9be84e5ededd34c3ad7
mpuhambyah <mpu.hambyah@gmail.com>
2021-11-12 12:56:43 +0700

    ts

--- application/views/pasien/gantiPassword.php
+++ application/views/pasien/gantiPassword.php
@@ -14,22 +14,22 @@
                     <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3" for="current_password">Kata Sandi Lama</label>
                     <div class="col-sm-12 col-md-7">
                         <input type="password" class="form-control" id="current_password" name="current_password">
+                        <?= form_error('current_password', '<small class="text-danger">', '</small>'); ?>
                     </div>
-                    <?= form_error('current_password', '<small class="text-danger">', '</small>'); ?>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3" for="new_password1">Kata Sandi Baru</label>
                     <div class="col-sm-12 col-md-7">
                         <input type="password" class="form-control" id="new_password1" name="new_password1">
+                        <?= form_error('new_password1', '<small class="text-danger">', '</small>'); ?>
                     </div>
-                    <?= form_error('new_password1', '<small class="text-danger">', '</small>'); ?>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3" for="new_password2">Ulangi Kata Sandi</label>
                     <div class="col-sm-12 col-md-7">
                         <input type="password" class="form-control" id="new_password2" name="new_password2">
+                        <?= form_error('new_password2', '<small class="text-danger">', '</small>'); ?>
                     </div>
-                    <?= form_error('new_password2', '<small class="text-danger">', '</small>'); ?>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
