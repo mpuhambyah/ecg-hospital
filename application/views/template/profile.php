0e6556e6e5716ad1ca00de9423cd65455dab77be
mpuhambyah <mpu.hambyah@gmail.com>
2021-11-12 13:24:10 +0700

    ts

--- application/views/template/profile.php
+++ application/views/template/profile.php
@@ -4,7 +4,7 @@
             <h1>Profile</h1>
         </div>
         <div class="row">
-            <div class="col-12 col-md-6 col-lg-6">
+            <div class="col-12">
                 <div class="card author-box card-primary">
                     <div class="card-body">
                         <div class="author-box-left">
@@ -15,56 +15,21 @@
                             <div class="author-box-name">
                                 <a href="#"><?= $profileUser['nama'] ?></a>
                             </div>
-                            <div class="author-box-job"><?= $profileUser['role'] ?></div>
-                            <div class="author-box-description">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</p>
-                            </div>
+                            <div class="author-box-job"><?= $profileUser['role'] ?> / <?= $profileUser['email'] ?></div>
+                            <?php if ($profileUser['is_active'] == 1) { ?>
+                                <div class="author-box-job">AKTIF bergabung pada <?= date('d-m-Y H:i:s', $l['date_created']) ?></div>
+                            <?php } else { ?>
+                                <div class="author-box-job">TIDAK AKTIF bergabung pada <?= date('d-m-Y H:i:s', $l['date_created']) ?></div>
+                            <?php } ?>
+                            <?php if ($profileUser['alamat'] != NULL) { ?>
+                                <div class="author-box-description">
+                                    <p><?= $profileUser['alamat'] ?></p>
+                                </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
 
-            </div>
-            <div class="col-12 col-md-6 col-lg-6">
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h4>Input Text</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label>Nama</label>
-                            <input type="text" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Nomor Handphone</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fas fa-phone"></i>
-                                    </div>
-                                </div>
-                                <input type="text" class="form-control phone-number">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label>Alamat</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fas fa-map-marker"></i>
-                                    </div>
-                                </div>
-                                <input type="text" class="form-control currency">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label>Tanggal lahir</label>
-                            <input type="date" class="form-control datemask" placeholder="YYYY/MM/DD">
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
